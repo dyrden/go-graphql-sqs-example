d0da623ee4a1afd5573908c396af7d17b2848e57
Paul Schkade <paul.schkade@finanzcheck.de>
2019-12-04 23:27:00 +0100

    add comments, rename infrastructure to ops

--- ops/aws.tf
+++ ops/aws.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
+# create new sqs queue
 resource "aws_sqs_queue" "go-sqs-queue" {
   name                      = var.aws_sqs_queue_name
   delay_seconds             = 0
@@ -10,11 +11,13 @@ resource "aws_sqs_queue" "go-sqs-queue" {
   receive_wait_time_seconds = 0
 }
 
+# create new iam policy to use sqs queue
 resource "aws_iam_policy" "go-sqs-queue-policy" {
   name = "sqs-${var.aws_sqs_queue_name}-policy"
   policy = data.aws_iam_policy_document.go-sqs-queue-policy-doc.json
 }
 
+# policy data
 data "aws_iam_policy_document" "go-sqs-queue-policy-doc" {
   statement {
     actions = [
@@ -40,28 +43,34 @@ data "aws_iam_policy_document" "go-sqs-queue-policy-doc" {
   }
 }
 
+# create new iam user to use sqs queue
 resource "aws_iam_user" "go-sqs-iam-user" {
   name = var.aws_iam_user
 }
 
+# create new iam access key for that user
 resource "aws_iam_access_key" "go-sqs-iam-user" {
   user = aws_iam_user.go-sqs-iam-user.name
 }
 
+# output secret to store it in aws-vault
 output "secret" {
   value = "${aws_iam_access_key.go-sqs-iam-user.secret}"
 }
 
+# connect user with policy for access rights (!Achtung!)
 resource "aws_iam_user_policy_attachment" "go-sqs-iam-user-attachment" {
   user = aws_iam_user.go-sqs-iam-user.name
   policy_arn = aws_iam_policy.go-sqs-queue-policy.arn
 }
 
+# create iam role
 resource "aws_iam_role" "go-sqs-iam-role" {
   name = "go-graphql-sqs-example-role"
   assume_role_policy = data.aws_iam_policy_document.go-sqs-iam-role-policy.json
 }
 
+# role data (relationship between sqs user and role)
 data "aws_iam_policy_document" "go-sqs-iam-role-policy" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -73,6 +82,7 @@ data "aws_iam_policy_document" "go-sqs-iam-role-policy" {
   }
 }
 
+# connect role with policy
 resource "aws_iam_role_policy_attachment" "go-sqs-iam-role-attach" {
   role = aws_iam_role.go-sqs-iam-role.name
   policy_arn = aws_iam_policy.go-sqs-queue-policy.arn
--- readme.md
+++ readme.md
@@ -8,4 +8,30 @@ requires:
 - create access permissions
 - set AWS credentials via ENV / env file if using docker
 
+## Getting started
 
+ops/$ make init
+ops/$ make plan
+ops/$ make apply
+
+$ make install_dependencies
+$ make run_as_iam_role
+
+GraphQl:
+send 
+```
+mutation {
+  sendMessage(msg: "hey")
+}
+
+```
+receive:
+```
+{
+  receiveMessage
+}
+```
+
+
+bindata
+$ go-bindata -o bindata2.go graphiql.html schema.gql 
